179624b40735a401cbb8b4d02ce8d493b4e96026
Carlos María Cornejo Crespo <cmcornejo@atsistemas.com>
2017-11-29 12:35:33 +0100

    Added docker/machine support to Vagrant

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -7,3 +7,6 @@ curl -sSL https://get.docker.com/ | sh
 usermod -aG docker $USER
 curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+chmod +x /tmp/docker-machine &&
+sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
